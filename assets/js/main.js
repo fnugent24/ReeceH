7e3b5b21f00f7e9af92870a3b06afdc920d184dc
fnugent24 <119634822+fnugent24@users.noreply.github.com>
2025-04-30 19:44:31 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -374,4 +374,23 @@ $('#toggle').click(function() {
      $(".thanks").css("opacity", 0);
   }
 });
+
+	$('.share-button').on('click',function(){  
+  $(this).addClass('open');
+})
+$( ".share-items" ).draggable({ 
+  axis: "x",
+  containment : ".share-items-wrapper"
+});
+$( ".share-item" ).on('click',function(){
+   $('.share-button').addClass('shared');
+   setTimeout(function(){
+    $('.share-button').addClass('thankyou');
+  }, 800);
+  setTimeout(function(){
+    $('.share-button').removeClass('open');
+    $('.share-button').removeClass('shared');
+    $('.share-button').removeClass('thankyou');
+  }, 2500);
+});
  })(jQuery);
