4ce1e5b6a999c925ba8909793f305e62102be29b
fnugent24 <119634822+fnugent24@users.noreply.github.com>
2025-04-30 19:25:04 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -360,5 +360,18 @@
     })
 
 });
+// $(".button").blur(function() {
+//   alert("lost");
+// });
 
+$('#toggle').click(function() {
+  if ($(this).is(":checked")) {
+    $(".sub").css("opacity", 0);
+    $(".thanks").css("opacity", 1);
+  } 
+  else if ($(this).is(":not(:checked)")) {
+     $(".sub").css("opacity", 1);
+     $(".thanks").css("opacity", 0);
+  }
+});
  })(jQuery);
